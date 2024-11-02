b421fb39ae4d40e024270124f2607ec747388a5b
unknown <aavgustin@veleri.hr>
2024-11-02 15:40:24 +0100

    Opis Aplikacije

---
+++ frontend/quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,112 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+defineOptions({
+  name: 'MainLayout'
+})
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Naslovnica',
+    caption: 'Naslovnica',
+    icon: 'book',
+    link: '#/'
+  },
+  {
+    title: 'Popis knjiga',
+    caption: 'Popis knjiga u knjiznici',
+    icon: 'book',
+    link: '#/popisKnjiga'
+  },
+  {
+    title: 'Pretrazivanje',
+    caption: 'Pretrazivanje',
+    icon: 'book',
+    link: '#/TraziKnjigu'
+  },
+  {
+    title: 'O nama',
+    caption: 'O nama',
+    icon: 'rss_feed',
+    link: '#/ONama'
+  },
+  {
+    title: 'Lokacija',
+    caption: 'Lokacija',
+    icon: 'record_voice_over',
+    link: '#/Lokacija'
+  },
+  {
+    title: 'Login',
+    caption: 'Login',
+    icon: 'code',
+    link: '#/Login'
+  },
+  {
+    title: 'Registracija',
+    caption: 'Registracija',
+    icon: 'code',
+    link: '#/Registracija'
+  }
+]
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
---
+++ frontend/quasar-project/src/router/routes.js
@@ -0,0 +1,25 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/popisKnjiga', component: () => import('pages/PopisKnjigaPage.vue')},
+      { path: '/TraziKnjigu', component: () => import('pages/TraziKnjiguPage.vue')},
+      { path: '/ONama', component: () => import('pages/ONamaPage.vue')},
+      { path: '/Lokacija', component: () => import('pages/LokacijaPage.vue')},
+      { path: '/Login', component: () => import('pages/LoginPage.vue')},
+      { path: '/Registracija', component: () => import('pages/RegistracijaPage.vue')}
+
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes

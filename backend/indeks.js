68af550c1abff69459edcd66f7bffcc81ddebd9c
unknown <aavgustin@veleri.hr>
2024-11-21 11:21:08 +0100

    backend

---
+++ backend/indeks.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const app = express();
+const port = 3000;
+
+// Parser za JSON podatke
+app.use(bodyParser.json());
+
+// Parser za podatke iz formi
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+    host: 'ucka.veleri.hr',
+    user: 'aavgustinovic',
+    password: '11',
+    database: 'aavgustinovic'
+  });
+  
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/knjige", (req, res) => {
+    connection.query("SELECT * FROM knjige", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+app.get("/api/knjige/:naslov", (req, res) => {
+    connection.query("SELECT * FROM knjiga WHERE naslov like "%naziv%"", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+}); 
+app.get("/api/knjige/:autor", (req, res) => {
+    connection.query("SELECT (knjiga.stanje - count(rezervacija.knjiga)) as slobodne, knjiga.id, knjiga.naslov, knjiga.stanje FROM `knjiga` left join rezervacija on knjiga.id=rezervacija.knjiga group by knjiga.id", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+app.get("/api/slob_knjige/:id_knjige", (req, res) => {
+    connection.query("SELECT * FROM knjiga WHERE id_knjige like "%id_knjige%"", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+app.get("/api/rezerv_knjige", (req, res) => {
+    connection.query("/api/rezerv_knjige	GET	Lista rezerviranih knjiga	SELECT * FROM knjiga, rezervacija WHERE knjiga.id=rezervacija.knjiga", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+app.get("/api/rezerv_knjige_korisnici", (req, res) => {
+    connection.query("/api/rezerv_knjige_korisnici	GET	Lista rezerviranih knjiga s korisnicima	SELECT * FROM knjiga, rezervacija, korisnik WHERE knjiga.id=rezervacija.knjiga and korisnik.id=rezervacija.korisnik", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+app.get("/api/rezerv_knjige/:id_korisnik", (req, res) => {
+    connection.query("SELECT * FROM knjiga, rezervacija, korisnik WHERE knjiga.id=rezervacija.knjiga and korisnik.id=rezervacija.korisnik AND korisnik.id=id_korisnik", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+app.get("/api/rezerv_knjige/:id_knjiga", (req, res) => {
+    connection.query("SELECT * FROM knjiga, rezervacija, korisnik WHERE knjiga.id=rezervacija.knjiga and korisnik.id=rezervacija.korisnik AND knjiga.id=id_knjiga", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+app.get("/api/korisnici", (req, res) => {
+    connection.query("SELECT * FROM korisnici", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+app.get("/api/korisnici/:id_korisnik", (req, res) => {
+    connection.query("SELECT id_korisnik FROM korisnici", (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  app.listen(port, () => {
+    console.log("Server running at port: " + port);
+});
--- frontend/quasar-project/src/pages/RegistracijaPage.vue
+++ frontend/quasar-project/src/pages/RegistracijaPage.vue
@@ -1,12 +1,12 @@
 <template>
-  <label for="name">Korisničko Ime:</label>
-  <input type="text" id="name" name="name">
-  <br>
-  <label for="password">Lozinka:</label>
-  <input type="password" id="password" name="password">
-  <div class="text-h3 text-weight-bolder text-center">Registracija</div>
+
   <q-page padding>
-    <!-- content -->
+    <label for="name">Korisničko Ime:</label>
+    <input type="text" id="name" name="name"/>
+     <br>
+    <label for="password">Lozinka:</label>
+    <input type="password" id="password" name="password"/>
+    <div class="text-h3 text-weight-bolder text-center">Registracija</div>
   </q-page>
 </template>
 
